1efe2ee2462be8d4e199c93d42735ea82b9e4ff7
Yusuf <baig.yusuf.cr7@gmail.com>
2021-05-28 08:26:15 +0530

    chomd

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="search-box">
+      <input type="text" placeholder="Search here..." />
+    </div>
+
+    <div class="nav-container">
+      <div class="wrapper">
+        <nav>
+          <div class="logo">
+            Feed the kitty
+
+          </div>
+
+          <ul class="nav-items">
+            <li>
+              <a href="login.php">login</a>
+            </li>
+
+            <li>
+              <a href="signup.php">signup</a>
+            </li>
+
+            <!-- <li>
+              <a href="#">about</a>
+            </li> -->
+
+            <li>
+              <a class="nav-btn-container" href="#">
+                <img class="search-btn" src="images/search-icon.svg" alt="" />
+                <img class="close-btn" src="images/close-icon.svg" alt="" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+
+    <div class="header-container">
+      <div class="wrapper">
+        <header>
+          <div class="hero-content">
+          <br><br><br><br><br><br><br><br><br><h1>We here at Feed the kitty,</h1>
+            <p>
+              Try to bring together donors and recipients, you can register for either.
+              All of our recipients go through proper authentification to make sure all of them are genuine<br>
+              Lets get started!  
+            </p>
+            <a href="#">
+              Contact Us
+            </a>
+          </div>
+
+          <div class="hero-image">
+            <img src="images/e.jpg" alt="" / height="530" width="530">
+            <div class="photo-bg"></div>
+          </div>
+        </header>
+
+        <div class="social-icons">
+          <a href="#">
+            <img src="images/instagram-logo.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="images/behance-logo.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="images/dribbble-logo.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <script src="main.js"></script>
+  </body>
+</html>
